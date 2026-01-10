f146daef748f5c6f3f50967300b5358f76e15f91
Archquise <archquise@gmail.com>
2026-01-10 20:38:37 +0400

    refactor: updated header of all modules: added ascii logo, hid license into a file, to make it more readable and friendly, removed useless scopes, added meta banners for limoka

--- Music.py
+++ Music.py
@@ -1,28 +1,18 @@
-# Proprietary License Agreement
+# ###########█▄█   █▀▄▀█ █▀█ █▀▄ █▀###########
+# ###########█ █ ▄ █ ▀ █ █▄█ █▄▀ ▄█###########
 
-# Copyright (c) 2024-29 CodWiz
+# ##### Copyright (c) 2024-2029 CodWiz #######
 
-# Permission is hereby granted to any person obtaining a copy of this software and associated documentation files (the "Software"), to use the Software for personal and non-commercial purposes, subject to the following conditions:
-
-# 1. The Software may not be modified, altered, or otherwise changed in any way without the explicit written permission of the author.
-
-# 2. Redistribution of the Software, in original or modified form, is strictly prohibited without the explicit written permission of the author.
-
-# 3. The Software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the author or copyright holder be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the Software or the use or other dealings in the Software.
-
-# 4. Any use of the Software must include the above copyright notice and this permission notice in all copies or substantial portions of the Software.
-
-# 5. By using the Software, you agree to be bound by the terms and conditions of this license.
-
-# For any inquiries or requests for permissions, please contact codwiz@yandex.ru.
-
+# 💬 Contact: https://t.me/shuseks
+# 🔒 Licensed under custom proprietary license.
+# 📄 LICENSE: https://raw.githubusercontent.com/archquise/H.Modules/main/LICENSE
 # ---------------------------------------------------------------------------------
 # Name: Music
 # Description: Searches for music using Telegram music bots
 # Author: @hikka_mods
+# ---------------------------------------------------------------------------------
 # meta developer: @hikka_mods
-# scope: Music
-# scope: Music 0.0.2
+# meta banner: https://raw.githubusercontent.com/archquise/hmods_meta/main/Music.png
 # ---------------------------------------------------------------------------------
 
 # Thanks to @murpizz for the search code yandex
@@ -84,7 +74,7 @@ class MusicMod(loader.Module):
 
     @loader.command(
         ru_doc="Найти трек в Yandex.Music: `.music {название}`",
-        en_doc="Find a track in Yandex.Music: `.music yandex {name}`",
+        en_doc="Find a track in Yandex.Music: `.music {name}`",
     )
     async def music(self, message):
         args = utils.get_args(message)
